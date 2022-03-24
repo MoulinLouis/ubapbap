97ddf81ad8584ed0cea86ff659a4e66fc66d6416
MoulinLouis <moulin.louis93@gmail.com>
2022-03-24 17:12:48 +0100

    :construction: Keep on kitsu page

--- Views/Characters/kitsu_view.php
+++ Views/Characters/kitsu_view.php
@@ -1,4 +1,21 @@
 <link rel="stylesheet" href="<?php App\Core\View::getAssets("characters.css")?>">
+<link rel="stylesheet" href="http://protip.rocks/protip.min.css">
+
+<?php
+$char_stats_trios = [
+    "nb_games" => 3454,
+    "kill_per_game" => round(3454 / 4494, 2),
+    "assist_per_game" => round(3454 / 3938, 2),
+    "death_per_game" => round(3454 / 4747, 2),
+    "pickrate" => round(0.08691 * 100, 2),
+    "win" => 512,
+    "avg_dmg" => round(21742.15953, 2),
+];
+$spell_m1 = "<h3><strong>ENERGY ARROW</strong></h1><strong>Shoot an energy arrow.</strong><br>Base damage : 200";
+$spell_a = "<h3><strong>CHARGED ARROW</strong></h1><strong>Shoot a charged arrow at a longer range. Apply slow on hit.</strong><br>Base damage : 60 every 0.5s<br>Base CD : 6 seconds";
+$spell_space = "<h3><strong>RECOIL BLAST</strong></h1><strong>Shoot the ground to jump away. Apply knock up on hit.</strong><br>Base damage : 120<br>CD : 6 seconds";
+$spell_e = "<h3><strong>ARROW MISSILE</strong></h1><strong>Shoot a big energy arrow that pierces obstacles and enemies. Apply slow on hit</strong><br>Base damage : 420<br>Base CD : 20 seconds";
+?>
 
 <div class="character-profile-main-page">
     <div class="character-profile-content-container">
@@ -21,7 +38,81 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="character-profile-page">
+                    <div class="character-build">
+                        <div class="character-skills" style="padding-bottom: 25px">
+                            <div class="character-skill-with-label protip" data-pt-gravity="top" data-pt-title="<?= $spell_m1 ?>">
+                                <img src="<?php App\Core\View::getAssets("spells/kitsu/spell_m1.png")?>" alt="">
+                                <div class="skill-label bottom-center">M1</div>
+                            </div>
+
+                            <div class="character-skill-with-label protip" data-pt-gravity="top" data-pt-title="<?= $spell_a ?>">
+                                <img src="<?php App\Core\View::getAssets("spells/kitsu/spell_a.png")?>" alt="">
+                                <div class="skill-label bottom-center">A</div>
+                                
+                            </div>
+                            <div class="character-skill-with-label protip" data-pt-gravity="top" data-pt-title="<?= $spell_space ?>">
+                                <img src="<?php App\Core\View::getAssets("spells/kitsu/spell_space.png")?>" alt="">
+                                <div class="skill-label bottom-center space">SPACE</div>
+                            </div>
+                            <div class="character-skill-with-label protip" data-pt-gravity="top" data-pt-title="<?= $spell_e ?>">
+                                <img src="<?php App\Core\View::getAssets("spells/kitsu/spell_e.png")?>" alt="">
+                                <div class="skill-label bottom-center">E</div>
+                            </div>
+                        </div>
+
+                        <div class="filter-manager">
+                            <div class="filter-width-wrapper">
+                                <div class="filter-manager_container">
+                                    <div class="filter-manager_label" style="padding-right: 10px;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 10"><path fill="#176dff" d="M17 2H1a1 1 0 010-2h16a1 1 0 011 1zm-4 4H5a1 1 0 010-2h8a1 1 0 011 1zM8 9a1 1 0 111 1z"></path></svg>
+                                        <span class="media-query">Filters</span>
+                                    </div>
+                                    <select name="" id="select-play-zone">
+                                        <option value="">Ranked Trios</option>
+                                        <option value="">Ranked Duos</option>
+                                        <option value="">Playtest #5</option>
+
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="content-section character-ranking-stats">
+                            <div class="win-rate">
+                                <div class="value"><?= $char_stats_trios["nb_games"] ?></div>
+                                <div class="label">games</div>
+                            </div>
+                            <div class="win-rate">
+                                <div class="value"><?= $char_stats_trios["win"] ?></div>
+                                <div class="label">Wins</div>
+                            </div>
+                            <div class="win-rate">
+                                <div class="value"><?= $char_stats_trios["kill_per_game"] ?></div>
+                                <div class="label">Kill per game</div>
+                            </div>
+                            <div class="win-rate">
+                                <div class="value"><?= $char_stats_trios["avg_dmg"] ?></div>
+                                <div class="label">Average damages</div>
+                            </div>
+                            <div class="win-rate">
+                                <div class="value"><?= $char_stats_trios["pickrate"] ?>%</div>
+                                <div class="label">Pick rate</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
 </div>
+
+<script src="http://protip.rocks/protip.min.js"></script>
+<script>
+$(document).ready(function(){
+    $.protip();
+});
+</script>
